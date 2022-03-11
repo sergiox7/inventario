dc25c6aab34548553ff2f3fc86665006fa0ca018
unknown <gbalderasm@gmail.com>
2022-03-11 16:36:41 -0600

    Creación API para CRUDS de productos

---
+++ application/controllers/Producto.php
@@ -0,0 +1,217 @@
+<?php
+defined('BASEPATH') OR exit('No se permite el acceso directo.');
+
+class Producto extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Producto_model');
+	}
+
+	public function getAll(){
+		//if($this->session->userdata('login') == true){
+
+			$respuesta = array();
+			$respuesta['resultado'] = 'false';
+			$respuesta['mensaje'] = 'Ocurrio un error durante la petición';
+			$respuesta['respuesta'] = null;
+
+			$res = $this->Producto_model->getAll();
+
+			if($res != NULL){
+				$respuesta['resultado'] = 'true';
+				$respuesta['mensaje'] = 'Registros obtenidos con éxito';
+				$respuesta['respuesta'] = $res;
+			}
+			
+			echo json_encode($respuesta);
+		//}
+	}
+
+	public function get(){
+		//if($this->session->userdata('login') == true){
+			$respuesta = array();
+			$respuesta['resultado'] = 'false';
+			$respuesta['mensaje'] = 'Ocurrio un error durante la petición';
+			$respuesta['respuesta'] = null;
+            
+            
+            $this->form_validation->set_data($_GET)->set_rules('id', 'id', 'trim|integer|max_length[11]|greater_than_equal_to[1]|required'); 
+		
+            if ($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/) {
+  				$id_registro        = $this->input->get('id'); 
+                $res = $this->Producto_model->getById($id_registro); 
+
+				if($res != NULL){
+
+				$respuesta['resultado'] = 'true';
+				$respuesta['mensaje'] = 'Registros obtenidos con éxito';
+				$respuesta['respuesta'] = $res;
+
+				}
+
+             }
+
+            /*Si la validación de campos es incorrecta*/
+            else {
+            	$this->form_validation->set_error_delimiters('','');
+				$respuesta['error'] = validation_errors();
+            }
+			
+            echo json_encode($respuesta);
+
+		//}
+	}
+
+
+	public function insert(){
+		//if($this->session->userdata('login') == true){
+
+			$respuesta = array();
+			$respuesta['resultado'] = 'false';
+			$respuesta['mensaje'] = 'Ocurrio un error durante la petición';
+			$respuesta['respuesta'] = null;
+
+			$this->form_validation->set_rules('unidadMedida', 'unidad de medida', 'required|htmlspecialchars|max_length[5]|trim');
+			$this->form_validation->set_rules('idProveedor', 'proveedor', 'required|integer|greater_than_equal_to[1]|max_length[11]|trim');
+			$this->form_validation->set_rules('nombre', 'nombre del producto', 'required|htmlspecialchars|max_length[50]|trim');
+			$this->form_validation->set_rules('precioVenta', 'precio del producto', 'required|numeric|greater_than[0]|trim');
+
+			if($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/){
+				$unidadMedida 	= $this->input->post("unidadMedida");
+				$idProveedor 	= $this->input->post("idProveedor");
+				$nombre 		= $this->input->post("nombre");
+				$precioVenta 	= $this->input->post("precioVenta");
+
+				$data = array(
+					"unidadMedida" 	=> $unidadMedida,
+					"idProveedor" 	=> $idProveedor,
+					"nombre" 		=> $nombre,
+					"precioVenta" 	=> $precioVenta,
+				);
+
+				$is_affected = $this->Producto_model->insert($data);
+
+				if($is_affected != NULL){
+					$respuesta['resultado'] = 'true';
+					$respuesta['mensaje'] = 'El registro se insertó correctamente';
+				}
+
+			}
+
+            /*Si la validación de campos es incorrecta*/
+            else {
+            	$this->form_validation->set_error_delimiters('','');
+				$respuesta['error'] = validation_errors();
+            }
+			
+            echo json_encode($respuesta);
+
+		//}
+	}
+
+
+	public function edit(){
+		//if($this->session->userdata('login') == true){
+
+			$respuesta = array();
+			$respuesta['resultado'] = 'false';
+			$respuesta['mensaje'] = 'Ocurrio un error durante la petición';
+			$respuesta['respuesta'] = null;
+
+			$this->form_validation->set_rules('idProducto', 'id del producto', 'trim|integer|max_length[11]|greater_than_equal_to[1]|required');
+			$this->form_validation->set_rules('unidadMedida', 'unidad de medida', 'required|htmlspecialchars|max_length[5]|trim');
+			$this->form_validation->set_rules('idProveedor', 'proveedor', 'required|numeric|greater_than_equal_to[1]|max_length[11]|trim');
+			$this->form_validation->set_rules('nombre', 'nombre del producto', 'required|htmlspecialchars|max_length[50]|trim');
+			$this->form_validation->set_rules('precioVenta', 'precio del producto', 'required|numeric|greater_than[0]|trim');
+
+			if($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/){
+				$idProducto		= $this->input->post("idProducto");
+				$unidadMedida 	= $this->input->post("unidadMedida");
+				$idProveedor 	= $this->input->post("idProveedor");
+				$nombre 		= $this->input->post("nombre");
+				$precioVenta 	= $this->input->post("precioVenta");
+
+				//revisar que exista el registro
+				$res = $this->Producto_model->getById($idProducto); 
+
+				if($res != NULL){
+					$data = array(
+						"unidadMedida" 	=> $unidadMedida,
+						"idProveedor" 	=> $idProveedor,
+						"nombre" 		=> $nombre,
+						"precioVenta" 	=> $precioVenta,
+					);
+
+					$is_affected = $this->Producto_model->update($data, $idProducto);
+
+					if($is_affected != NULL){
+						$respuesta['resultado'] = 'true';
+						$respuesta['mensaje'] = 'El registro se actualizó correctamente';
+					}else{
+						$respuesta['mensaje'] = 'No fue posible modificar el registro';
+					}
+
+				}
+
+			}
+
+            /*Si la validación de campos es incorrecta*/
+            else {
+            	$this->form_validation->set_error_delimiters('','');
+				$respuesta['error'] = validation_errors();
+            }
+			
+            echo json_encode($respuesta);
+
+		//}
+	}
+
+
+	public function delete(){
+		//if($this->session->userdata('login') == true){
+
+			$respuesta = array();
+			$respuesta['resultado'] = 'false';
+			$respuesta['mensaje'] = 'Ocurrio un error durante la petición';
+			$respuesta['respuesta'] = null;
+
+			$this->form_validation->set_rules('idProducto', 'id del producto', 'trim|integer|max_length[11]|greater_than_equal_to[1]|required');
+
+			if($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/){
+				$idProducto		= $this->input->post("idProducto");
+
+				//revisar que exista el registro
+				$res = $this->Producto_model->getById($idProducto); 
+
+				if($res != NULL){
+
+					$is_affected = $this->Producto_model->deleteById($idProducto);
+
+					if($is_affected != NULL){
+						$respuesta['resultado'] = 'true';
+						$respuesta['mensaje'] = 'El registro se elimino correctamente';
+					}else{
+						$respuesta['mensaje'] = 'No fue posible eliminar el registro';
+					}
+
+				}
+
+			}
+
+            /*Si la validación de campos es incorrecta*/
+            else {
+            	$this->form_validation->set_error_delimiters('','');
+				$respuesta['error'] = validation_errors();
+            }
+			
+            echo json_encode($respuesta);
+
+		//}
+	}
+
+}
+
+
+
+
+?>
---
+++ application/models/Producto_model.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No se permite el acceso directo.');
+
+class Producto_model extends CI_Model{
+
+	public function getAll(){
+		$this->db->join('proveedor', 'proveedor.idProveedor = producto.idProveedor');
+		$rs = $this->db->get('producto');
+		return $rs->num_rows() > 0 ? $rs->result() : NULL;
+	}
+
+     public function getById($id)
+    {
+        $cmd = "SELECT * FROM producto where idProducto = ".$id;
+
+        $query = $this->db->query($cmd);
+        return ($query->num_rows() == 1) ? $query->row() : NULL;
+    }
+
+	public function insert($data){
+		return $this->db->insert('producto', $data) ? true : NULL;
+	}
+
+
+    public function update($entry, $idProducto)
+    {
+        try {
+            $this->db->set($entry);
+            $this->db->where('idProducto', $idProducto);
+            $this->db->update('producto');
+
+            return ($idProducto) ? TRUE : NULL;
+        }
+
+        catch(Exception $e) {
+            return $e->getMessage();
+        }
+    }	
+
+     public function deleteById($id)
+    {
+        $cmd = "DELETE FROM producto where idProducto = ".$id;
+
+        $query = $this->db->query($cmd);
+        return (TRUE) ? TRUE : NULL;
+    }
+     
+}
+
+
+/*
+{
+	"table": "producto",
+	"rows":
+	[
+		{
+			"idProducto": 1,
+			"unidadMedida": "lts",
+			"idProveedor": 1,
+			"nombre": "producto 1",
+			"precioVenta": 1
+		}
+	]
+}
+
+*/
